f38e82db909cef23814598eab8c87cb62362cff0
Michael P <102651473+michmich16@users.noreply.github.com>
2024-08-28 08:55:01 +0200

    lg

--- pizza_spil/src/components/Buttons/Buttons.jsx
+++ pizza_spil/src/components/Buttons/Buttons.jsx
@@ -5,7 +5,7 @@ export const Buttons=()=>{
     return(
         <div className={s.buttonsStyle}>
         <button className={s.lowerButtonStyle}>Lower</button>
-        <button className={s.equalButtonStyle}>Equal</button>
+        <button className={s.equalButtonStyle}>Six</button>
         <button className={s.higherButtonStyle}>Higher</button>
         </div>
     )
--- pizza_spil/src/components/Dice.jsx
+++ pizza_spil/src/components/Dice.jsx
@@ -1,26 +1,25 @@
 import { useEffect, useState } from 'react';
 import style  from '../style/Dice.module.scss'
-import 
 
-function Dice (props){
+ function Dice (props){
 
-    const [ number, setNumber] =  useState(1);
+//     const [ number, setNumber] =  useState(1);
 
-    useEffect(()=>{
-        generateNum();
-    }, []);
+//     useEffect(()=>{
+//         generateNum();
+//     }, []);
 
-    const generateNum = () => {
-     const randomNumber = Math.floor(Math.random()*6)+1;
-     setNumber(randomNumber);
+//     const generateNum = () => {
+//      const randomNumber = Math.floor(Math.random()*6)+1;
+//      setNumber(randomNumber);
 
-    };
+//     };
 
 
     return(
         <div>
-            <div className={style.diceStyle} onClick={generateNum} >
-                <div className={style.diceNumber}>{number}</div>
+            <div className={style.diceStyle} >
+                <div className={style.diceNumber}></div>
             </div>
         </div>
     )
